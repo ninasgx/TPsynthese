4f52b0ca08f245e33c639e6949783e4b01e091e1
ninasgx <sabrinasantosx@yahoo.com>
2024-11-28 09:30:24 +0100

    Step 1 correction: Display message with WRITE function and not PRINTF

--- tp1step1.c
+++ tp1step1.c
@@ -1,11 +1,16 @@
-#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
 int main() {
 
-    printf("Welcome to ENSEA (Alamo and Sabrina) Tiny Shell.\n");
-    printf("Type 'exit' to quit.\n");
+    const char *welcome_message = "Welcome to ENSEA (Alamo and Sabrina) Tiny Shell.\n";
+    write(STDOUT_FILENO, welcome_message, strlen(welcome_message));
 
-    printf("enseash %% ");
+    const char *exit_message = "Type 'exit' to quit.\n";
+    write(STDOUT_FILENO, exit_message, strlen(exit_message));
+
+    const char *prompt = "enseash %% ";
+    write(STDOUT_FILENO, prompt, strlen(prompt));
 
     return 0;
 }
