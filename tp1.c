4c934a437a89a3c64e8602c311d9881ef465a30e
ninasgx <sabrinasantosx@yahoo.com>
2024-11-28 10:34:44 +0100

    Step 2: Execution of the entered command and return to the prompt

--- tp1.c
+++ tp1.c
@@ -1,16 +1,45 @@
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAX_INPUT_LENGTH 1024 
 
 int main() {
+    char command[MAX_INPUT_LENGTH];
+    pid_t pid;  // Child ID
+    int status;  // Child exit status
 
     const char *welcome_message = "Welcome to ENSEA (Alamo and Sabrina) Tiny Shell.\n";
     write(STDOUT_FILENO, welcome_message, strlen(welcome_message));
 
-    const char *exit_message = "Type 'exit' to quit.\n";
-    write(STDOUT_FILENO, exit_message, strlen(exit_message));
+    while (1) {
+        write(STDOUT_FILENO, "enseash % ", 10);
 
-    const char *prompt = "enseash %% ";
-    write(STDOUT_FILENO, prompt, strlen(prompt));
+        if (fgets(command, MAX_INPUT_LENGTH, stdin) == NULL) {
+            write(STDOUT_FILENO, "Error reading command.\n", 23);
+            continue;
+        }
 
+        command[strcspn(command, "\n")] = 0; // Remove the newline character
+        pid = fork(); // Create a child process to execute the command
+        
+        if (pid == -1) {
+            write(STDOUT_FILENO, "Fork failed.\n", 13);
+            continue;
+        }
+
+        if (pid == 0) {
+            char *args[] = {command, NULL};  // Arguments for execvp
+            if (execvp(command, args) == -1) {
+                write(STDOUT_FILENO, "Command execution failed.\n", 25);
+                exit(1);
+            }
+        } else {
+            waitpid(pid, &status, 0); // Parent wait for the child process to finish
+        }
+    }
     return 0;
 }
