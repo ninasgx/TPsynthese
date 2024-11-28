9dfad556fcf172e4f041740ba0fbf2b40d2b9be4
Sabrina Santos <sabrinasantos@MacBook-Pro-de-Sabrina.local>
2024-11-28 08:35:17 +0100

    Step 1: Display a welcome message

---
+++ tp1step1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("Welcome to ENSEA (Alamo and Sabrina) Tiny Shell.\n");
+    printf("Type 'exit' to quit.\n");
+
+    printf("enseash %% ");
+
+    return 0;
+}
